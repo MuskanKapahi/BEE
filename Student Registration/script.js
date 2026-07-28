1a7d7f28d2bdab2d9310069841daab2c73b18b17
Muskan <muskankapahi5@gmail.com>
2026-07-28 17:01:23 +0530

    Added Assignment1, Assignment2

---
+++ Student Registration/script.js
@@ -0,0 +1,53 @@
+const form = document.getElementById("form");
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const age = document.getElementById("age");
+
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const ageError = document.getElementById("ageError");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    // Clear previous errors
+    nameError.textContent = "";
+    emailError.textContent = "";
+    ageError.textContent = "";
+
+    let isValid = true;
+
+    // Validate Name
+    if (name.value.trim() === "") {
+        nameError.textContent = "Name is required";
+        isValid = false;
+    }
+
+    // Validate Email
+    if (email.value.trim() === "") {
+        emailError.textContent = "Email is required";
+        isValid = false;
+    }
+    else if (!email.value.includes("@")) {
+        emailError.textContent = "Enter a valid email";
+        isValid = false;
+    }
+
+    // Validate Age
+    if (age.value.trim() === "") {
+        ageError.textContent = "Age is required";
+        isValid = false;
+    }
+    else if (age.value <= 0) {
+        ageError.textContent = "Enter a valid age";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Form Submitted Successfully!");
+        form.reset();
+    }
+
+});
